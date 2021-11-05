63ed86bbc822822bafe98ebd54e9a34b1a1c5385
이아현 <suk03029@gmail.com>
2021-11-05 16:18:33 +0900

    프로필 사진을 누르면 실제로 크게 보기 화면을 만듬 / 그 화면만 액션바를 제거함

--- app/src/main/java/com/nepplus/librarypractice20211105/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice20211105/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.nepplus.librarypractice20211105
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvent()
+        setupValues()
+    }
+
+    fun setupEvent(){
+
+//        이미지 뷰나 txt 뷰/ LinearLayout등은 버튼처럼 setOnClickListener가능
+        imgProfile.setOnClickListener {
+
+            val myIntent=Intent(this,ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
+    }
+    fun setupValues(){
+
     }
 }
