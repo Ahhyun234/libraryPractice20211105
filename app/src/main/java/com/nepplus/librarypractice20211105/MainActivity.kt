197effff4057ab5ef51314e931dee800afdf6dc7
이아현 <suk03029@gmail.com>
2021-11-05 17:05:06 +0900

    ted permission을 이용한 작업 진행 중 - Library설치 완료 - menifest 에 uses 명시 - permisionListsner를 작성중(권한 획득 여부에 따른 행동 방침)

--- app/src/main/java/com/nepplus/librarypractice20211105/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice20211105/MainActivity.kt
@@ -3,6 +3,8 @@ package com.nepplus.librarypractice20211105
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,18 +16,38 @@ class MainActivity : AppCompatActivity() {
         setupValues()
     }
 
-    fun setupEvent(){
+    fun setupEvent() {
 
 //        이미지 뷰나 txt 뷰/ LinearLayout등은 버튼처럼 setOnClickListener가능
         imgProfile.setOnClickListener {
 
-            val myIntent=Intent(this,ViewPhotoActivity::class.java)
+            val myIntent = Intent(this, ViewPhotoActivity::class.java)
             startActivity(myIntent)
+//      권한 획득
+//      1) 권한 여하에 따른 상황별 대처 방안. 미리 변수에 담아두자
+            val performedListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+//                    권한이 획득 되었을 때 실행 할 코드
+                    
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+//                    권한이 거부 되었을 때 실행 할 코드
+                    Toast.makeText(this@MainActivity, "권한이 거부되어 전화연결이 불가합니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
 
         }
 
     }
-    fun setupValues(){
+
+    fun setupValues() {
+
+        btnCall.setOnClickListener {
+
+        }
 
     }
 }
