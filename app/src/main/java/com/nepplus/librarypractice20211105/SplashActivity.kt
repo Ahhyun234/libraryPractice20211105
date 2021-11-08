718ea8d17635e89e887a8be7844e6f51e0c917dc
이아현 <suk03029@gamil.com>
2021-11-08 10:56:58 +0900

    2.5초 후에 메인 화면으로 넘어가도록 핸들러를 사용 해 봄

--- app/src/main/java/com/nepplus/librarypractice20211105/SplashActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice20211105/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.nepplus.librarypractice20211105
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +22,16 @@ class SplashActivity : AppCompatActivity() {
     }
 
     fun setValues(){
+
+//        2.5초 후에 메인 화면으로 이동 시키고 종료
+
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+
+          val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+
+        },2500)
 
     }
 }
