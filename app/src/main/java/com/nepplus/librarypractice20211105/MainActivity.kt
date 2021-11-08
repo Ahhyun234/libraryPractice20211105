919b0a8f7186412d8c7b228c665b9186d491d154
이아현 <suk03029@gamil.com>
2021-11-08 10:16:12 +0900

    Glide 라이브러리 사용법 체험

--- app/src/main/java/com/nepplus/librarypractice20211105/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice20211105/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
@@ -61,6 +62,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupValues() {
+
+        Glide.with(this).load("https://i.ytimg.com/vi/NLy6UdpBlPw/maxresdefault.jpg").into(imgAction)
 
         btnCall.setOnClickListener {
 
